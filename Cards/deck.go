8ef5ccafd98b6d0868cd21ec9fbadded32153b72
Gabriel H <gabrielh92@gmail.com>
2021-05-04 00:03:01 -0400

    24: added newDeck and deal functions

--- Cards/deck.go
+++ Cards/deck.go
@@ -4,8 +4,25 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	suits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cards := deck{}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, num int) (deck, deck) {
+	return d[:num], d[num:]
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,9 +1,13 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spades", newCard()}
-
+	cards := newDeck()
 	cards.print()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	remainingCards.print()
 }
 
 func newCard() string {
