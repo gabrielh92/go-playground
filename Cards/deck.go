b6a2e0df2a2d15a8b1eb329726ff0aaa8d60649f
Gabriel H <gabrielh92@gmail.com>
2021-05-06 00:02:44 -0400

    32: wrapped up basic deck functions

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
 func newDeck() deck {
 	suits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
-	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six",
+		"Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
 	cards := deck{}
 
 	for _, suit := range suits {
@@ -14,6 +23,7 @@ func newDeck() deck {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
+
 	return cards
 }
 
@@ -25,4 +35,32 @@ func (d deck) print() {
 
 func deal(d deck, num int) (deck, deck) {
 	return d[:num], d[num:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(255)
+	}
+
+	return deck(strings.Split(string(bs), ", "))
+}
+
+func (d deck) shuffle() {
+	seed := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(seed)
+	for i := range d {
+		newIdx := rng.Intn(len(d) - 1)
+		d[i], d[newIdx] = d[newIdx], d[i]
+	}
 }
